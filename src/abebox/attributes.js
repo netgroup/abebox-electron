a0b3b9a27016c22f416d6f4d6d07b9e6140db978
Emanuele Raso <emanuele.raso@gmail.com>
2021-11-29 17:26:41 +0100

    Separate attributes

--- src/abebox/attributes.js
+++ src/abebox/attributes.js
@@ -1,104 +1,84 @@
 const fs = require("fs");
+const core = require("./core");
 
 //const attrs_file_rel_path = "attributes/attributes_list.json";
 
-const _attributes_conf = {};
+const _attributes = {};
 
-const init = function(attribute_file) {
-  attributes_conf.file = attribute_file;
+const _compress_list = function() {
+  return _attributes.list.map((el) => el.id.toString());
 };
 
-const get_attrs = function() {
-  const attr_list_file = _conf.remote + attrs_file_rel_path;
-  if (!fs.existsSync(attr_list_file)) {
+const _get_attr_id = function(attr) {
+  return `${attr.univ}:${attr.attr}:v${attr.vers}`;
+};
+
+const _save = function() {
+  const attrs_obj = {
+    attributes: _attributes.list,
+  };
+  const attrs_jwt = core.generate_jwt(attrs_obj);
+  fs.writeFileSync(_attributes.file, attrs_jwt);
+};
+
+const init = function(attribute_file) {
+  _attributes.file = attribute_file;
+  _attributes.list = get_all();
+};
+
+const get_all = function() {
+  if (!fs.existsSync(_attributes_conf.file)) {
     return [];
   } else {
     const attrs_obj = core.verify_jwt(
-      fs.readFileSync(attr_list_file).toString()
+      fs.readFileSync(_attributes.file, "utf-8").toString()
     );
-    return attrs_obj.attributes;
+    _attributes.list = attrs_obj.attributes;
+    return _attributes.list;
   }
 };
 
-const _compress_list = function(attr_list) {
-  return attr_list.map((el) => el.id.toString());
-};
-
-const new_attr = function(new_obj) {
-  if (!_conf.configured) throw Error("ABEBox not configured");
-  if (!_conf.isAdmin) throw Error("To Add an Attribute need to be admin");
-
-  const attrs = get_attrs();
-
+const add = function(new_attr) {
   // Check if already exists
-  const index = attrs.findIndex((item) => item.id == new_obj.id);
+  const index = _attributes.list.findIndex(
+    (item) => _get_attr_id(item) == _get_attr_id(new_attr)
+  );
   if (index >= 0) {
-    throw Error("ID is already present");
+    throw Error("Attribute already exists");
   } else {
     // Add new
-    new_obj.id = attrs.length + 1;
-    attrs.push(new_obj);
-    const attrs_obj = {
-      attributes: attrs,
-    };
-    const attrs_jwt = core.generate_jwt(attrs_obj);
-    fs.writeFileSync(_conf.remote + "/" + attrs_file_rel_path, attrs_jwt);
-    const attrs_comp = _compress_list(attrs);
-    _conf.keys.abe.sk == core.create_abe_sk(attrs_comp);
+    _attributes.list.push(new_attr);
+    _save();
   }
-  return attrs;
+  return _attributes.list;
 };
 
-const set_attr = async function(new_obj) {
-  if (!_conf.configured) throw Error("ABEBox not configured");
-  if (!_conf.isAdmin) throw Error("To Modify an Attribute need to be admin");
-
-  console.log(`SET_ATTR ${new_obj.toString()}`);
-
-  const attrs = await get_attrs();
+const set = async function(attr) {
   // Check if already exists
-  const index = attrs.findIndex((item) => item.id == new_obj.id);
+  const index = _attributes.list.findIndex(
+    (item) => _get_attr_id(item) == _get_attr_id(new_attr)
+  );
   if (index < 0) {
-    throw Error("ID not present");
+    throw Error("Attribute not present");
   } else {
     // Replace
-    const rem = attrs.splice(index, 1);
-    console.log("Removing:", rem, attrs);
-    attrs.push(new_obj);
-    const attrs_obj = {
-      attributes: attrs,
-    };
-    const attrs_jwt = core.generate_jwt(attrs_obj);
-    fs.writeFileSync(
-      _conf.remote + "/" + attrs_file_rel_path,
-      attrs_jwt //JSON.stringify(attrs)
-    );
-    console.log("Adding:", new_obj, attrs);
+    _attributes.list[index] = attr;
+    _save();
   }
-  return attrs;
+  return _attributes.list;
 };
 
-const del_attr = async function(id) {
-  console.log(`DEL_ATTR ${id.toString()}`);
-
-  const attrs = await get_attrs();
+const del = async function(attr) {
   // Check if already exists
-  const index = attrs.findIndex((item) => item.id == id);
+  const index = _attributes.list.findIndex(
+    (item) => _get_attr_id(item) == _get_attr_id(attr)
+  );
   if (index < 0) {
-    throw Error("ID not present");
+    throw Error("Attribute not present");
   } else {
     // Remove
-    const rem = attrs.splice(index, 1);
-    const attrs_obj = {
-      attributes: attrs,
-    };
-    const attrs_jwt = core.generate_jwt(attrs_obj);
-    fs.writeFileSync(
-      _conf.remote + "/" + attrs_file_rel_path,
-      attrs_jwt //JSON.stringify(attrs)
-    );
-    console.log("Removing:", rem, attrs);
-    _conf.keys.abe.sk == core.create_abe_sk(attrs);
+    const rem = _attributes.list.splice(index, 1);
+    _save();
   }
-  return attrs;
+  return _attributes.list;
 };
