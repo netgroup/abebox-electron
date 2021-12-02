d46caac69779b36c85ab04cebc6e0ccc6fb04af6
Lorenzo Bracciale <lorenzo.bracciale@gmail.com>
2021-12-02 15:59:04 +0100

    fixed attribute test

--- test/attribute.test.js
+++ test/attribute.test.js
@@ -1,6 +1,6 @@
 const assert = require("assert");
 const fs = require("fs");
-const attribute = require("../src/abebox/attribute");
+const AttributeManager = require("../src/abebox/attribute");
 
 const attr_data_1 = { univ: "UN", attr: "A", vers: "1" };
 const attr_data_2 = { univ: "UN", attr: "B", vers: "1" };
@@ -8,6 +8,7 @@ const attr_data_3 = { univ: "UN", attr: "C", vers: "1" };
 
 describe("Attribute tests", () => {
   it("serialize and de-serialize policy", () => {
+    const attribute = AttributeManager();
     const my_policy = [[attr_data_1, attr_data_2], [attr_data_3]];
     let policy_string = attribute.policy_as_string(my_policy);
     assert.equal(policy_string, '("UN:A:1" OR "UN:B:1") AND ("UN:C:1")');
